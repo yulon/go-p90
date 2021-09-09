bc06320f026f802ffab36b260d18730a7c179652
Yulon <yulon@me.com>
2021-09-09 14:01:11 +0800

    feat: add TestMultiConn()

--- conn_test.go
+++ conn_test.go
@@ -2,6 +2,7 @@ package p90
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -77,4 +78,100 @@ func TestConn(*testing.T) {
 
 	fmt.Println("client close once:", con.Close())
 	fmt.Println("client close twice:", con.Close())
+}
+
+func TestMultiConn(t *testing.T) {
+	pr, err := Listen("localhost:8830")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		defer pr.Close()
+
+		for {
+			con, err := pr.AcceptP90()
+			if err != nil {
+				t.Fatal(err)
+			}
+			go func() {
+				buf := make([]byte, 2048)
+
+				n, err := con.ReadPacket(buf)
+				if err != nil {
+					t.Fatal(err)
+				}
+				fmt.Println("server read packet:", string(buf[:n]))
+
+				fmt.Println("server write packet: 246")
+				_, err = con.WritePacket([]byte("246"))
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				n, err = con.Read(buf)
+				if err != nil {
+					t.Fatal(err)
+				}
+				fmt.Println("server read:", string(buf[:n]))
+
+				fmt.Println("server write: 000")
+				_, err = con.Write([]byte("000"))
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				fmt.Println("server close once:", con.Close())
+				fmt.Println("server close twice:", con.Close())
+			}()
+		}
+	}()
+
+	var wg sync.WaitGroup
+
+	pr2, err := Listen("localhost:8840")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+
+		go func() {
+			con, err := pr2.Dial("127.0.0.1:8830")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			fmt.Println("client write packet: 123")
+			_, err = con.WritePacket([]byte("123"))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			buf := make([]byte, 2048)
+			n, err := con.ReadPacket(buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fmt.Println("client read packet:", string(buf[:n]))
+
+			fmt.Println("client write: 111")
+			_, err = con.Write([]byte("111"))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			n, err = con.Read(buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fmt.Println("client read:", string(buf[:n]))
+
+			fmt.Println("client close once:", con.Close())
+			fmt.Println("client close twice:", con.Close())
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
